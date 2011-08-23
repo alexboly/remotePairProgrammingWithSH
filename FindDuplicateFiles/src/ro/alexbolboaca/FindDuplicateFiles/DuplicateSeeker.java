cbfdc5c33343fb9f9c1457cec9db652f9485c0b1
Alexandru Bolboaca <alexboly@gmail.com>
2011-08-23 12:55:10 +0300

    Removed duplication from DuplicateSeeker and moved it closer to tests

--- FindDuplicateFiles/src/ro/alexbolboaca/FindDuplicateFiles/DuplicateSeeker.java
+++ FindDuplicateFiles/src/ro/alexbolboaca/FindDuplicateFiles/DuplicateSeeker.java
@@ -1,12 +1,14 @@
 package ro.alexbolboaca.FindDuplicateFiles;
 
+import java.util.List;
+
 public class DuplicateSeeker {
 
 
-	public DuplicateSeeker(String folderPath) {
-		if(folderPath.equals("nonExistingFolder") || folderPath.equals("anotherNonExistingFolder"))
-		{
-			throw new RuntimeException("Folder " + folderPath + " doesn't exist.");
+	public DuplicateSeeker(String folderPath, List<String> folderPaths) {
+				
+		if(folderPaths.contains(folderPath)){
+			throw new RuntimeException("Folder " + folderPath + " doesn't exist.");			
 		}
 	}
 
--- FindDuplicateFiles/src/ro/alexbolboaca/FindDuplicateFiles/FindDuplicateFilesTests.java
+++ FindDuplicateFiles/src/ro/alexbolboaca/FindDuplicateFiles/FindDuplicateFilesTests.java
@@ -1,6 +1,10 @@
 package ro.alexbolboaca.FindDuplicateFiles;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 import org.junit.rules.*;
 
@@ -9,6 +13,15 @@ public class FindDuplicateFilesTests {
 
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
+	
+	List<String> nonExistentFolderPaths;
+
+	@Before
+	public void setup(){
+		nonExistentFolderPaths = new ArrayList<String>();
+		nonExistentFolderPaths.add("nonExistingFolder");
+		nonExistentFolderPaths.add("anotherNonExistingFolder");
+	}
 	
 	@Test
 	public void emptyFolderHasNoDuplicates()
@@ -19,16 +32,20 @@ public class FindDuplicateFilesTests {
 	
 	@Test
 	public void emptyFolderHasNoDuplicatesAlex(){
-		DuplicateSeeker seeker = new DuplicateSeeker("doesn't matter");
+
+		DuplicateSeeker seeker = new DuplicateSeeker("doesn't matter", nonExistentFolderPaths);
 		
 		boolean foundDuplicates = seeker.seek();
 		
 		assertFalse(foundDuplicates);
 	}
 	
-	@Test(expected=RuntimeException.class)
+	@Test
 	public void throwsExceptionIfFolderDoesntExist(){
-		DuplicateSeeker seeker = new DuplicateSeeker("nonExistingFolder");
+		expectedEx.expect(RuntimeException.class);
+		expectedEx.expectMessage("Folder nonExistingFolder doesn't exist");
+
+		DuplicateSeeker seeker = new DuplicateSeeker("nonExistingFolder", nonExistentFolderPaths);
 		
 		seeker.seek();
 	}
@@ -38,7 +55,7 @@ public class FindDuplicateFilesTests {
 		expectedEx.expect(RuntimeException.class);
 		expectedEx.expectMessage("Folder anotherNonExistingFolder doesn't exist");
 		
-		DuplicateSeeker seeker = new DuplicateSeeker("anotherNonExistingFolder");
+		DuplicateSeeker seeker = new DuplicateSeeker("anotherNonExistingFolder", nonExistentFolderPaths);
 		
 		seeker.seek();
 	}
