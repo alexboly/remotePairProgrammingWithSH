cbd93b03d94bd0681ec09b95394e06630ddd170c
Alexandru Bolboaca <alexboly@gmail.com>
2011-08-23 09:59:03 +0300

    Added the first test class

---
+++ FindDuplicateFiles/src/ro/alexbolboaca/FindDuplicateFiles/FindDuplicateFilesTests.java
@@ -0,0 +1,14 @@
+package ro.alexbolboaca.FindDuplicateFiles;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class FindDuplicateFilesTests {
+
+	@Test
+	public void test(){
+		assertTrue(true);
+	}
+	
+}
